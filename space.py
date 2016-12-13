9c5fce13c15ddf6044530d9b93901e42b6bddeb1
nongharunana <anongnat.rat@ku.th>
2016-12-13 16:53:38 +0700

    add score

--- space.py
+++ space.py
@@ -28,6 +28,7 @@ class SpaceGameWindow(arcade.Window):
         arcade.start_render()
         self.gold_sprite.draw()
         self.ship_sprite.draw()
+        arcade.draw_text(str(self.world.score),self.width - 30, self.height - 30,arcade.color.WHITE, 20)
 
     def animate(self, delta):
         self.world.animate(delta)
